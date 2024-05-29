63af347e29037d5f90c967a367fe16c9e0d88fa0
RohitBiradar <rohitpramilabiradar@gmail.com>
2024-05-29 11:24:49 +0530

    add Swap file

--- Swap.java
+++ Swap.java
@@ -2,14 +2,17 @@ public class Swap
 {
     public static void main(String args[])
     {
+        //initializing three variables
         int a = 5;
         int b = 10;
         int temp = 0;
 
+        //swaping the values
         temp = a;
         a = b;
         b = temp;
 
+        //printing the swapped values
         System.out.println("a = "+a+" , b = "+b);
     }
 }
